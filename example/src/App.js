5963d33fb91789885f036d8c624c8af0bead7d3a
Fatih Tasdemir <fatih.tasdemir@combiine.com>
2021-03-30 14:51:32 +0300

    chore: add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,89 @@
-import * as React from 'react'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Steve from 'react-native-steve'
 
-export default function App() {
-    const [result, setResult] = React.useState()
+const topics = [
+    {
+        emoji: '🍻',
+        text: 'Entertainment'
+    },
+    {
+        emoji: '🐈',
+        text: 'Cats'
+    },
+    {
+        emoji: '🦾',
+        text: 'Robots'
+    },
+    {
+        emoji: '🎉',
+        text: 'Party'
+    },
+    {
+        emoji: '🌍',
+        text: 'World'
+    },
+    {
+        emoji: '📚',
+        text: 'Books'
+    },
+    {
+        emoji: '👘',
+        text: 'Fashion'
+    },
+    {
+        emoji: '📱',
+        text: 'Applications'
+    },
+    {
+        emoji: '📸',
+        text: 'Photography'
+    },
+    {
+        emoji: '🧠',
+        text: 'Ideas'
+    },
+    {
+        emoji: '⚔️',
+        text: 'War'
+    },
+    {
+        emoji: '💼',
+        text: 'Business'
+    },
+    {
+        emoji: '🎭',
+        text: 'Theater'
+    },
+    {
+        emoji: '📮',
+        text: 'Job'
+    }
+]
 
-    React.useEffect(() => {
-        Steve.multiply(3, 7).then(setResult)
-    }, [])
+export default function App() {
+    const { topicContainer, topicText, title } = styles
+
+    const renderTopic = ({ item }) => {
+        const { emoji, text } = item
+        return (
+            <View style={topicContainer}>
+                <Text>
+                    {emoji}
+                </Text>
+                <Text style={topicText}>
+                    {text}
+                </Text>
+            </View>
+        )
+    }
 
     return (
         <View style={styles.container}>
-            <Text>
-                Result:
-                {result}
-            </Text>
+            <Steve
+                data={topics}
+                renderItem={renderTopic}
+                keyExtractor={item => item.text}/>
         </View>
     )
 }
@@ -24,12 +93,32 @@ App.displayName = 'App'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        backgroundColor: '#FFF',
         justifyContent: 'center'
     },
-    box: {
-        width: 60,
-        height: 60,
-        marginVertical: 20
+    topicContainer: {
+        borderWidth: 1,
+        borderColor: '#ecd9d9',
+        borderBottomWidth: 2,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        height: 38,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        margin: 5,
+        backgroundColor: '#FFF'
+    },
+    topicText: {
+        fontSize: 14,
+        fontWeight: '500',
+        marginLeft: 5
+    },
+    title: {
+        fontSize: 13,
+        color: 'rgb(134,130,119)',
+        marginBottom: 5,
+        marginLeft: 15,
+        fontWeight: '600'
     }
 })
