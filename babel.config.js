92fe357376ee4728588f2451a8047dae608e25c4
Fatih Tasdemir <fatih.tasdemir@combiine.com>
2021-03-30 14:32:02 +0300

    chore: remove babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-        'react-native-reanimated/plugin',
-        '@babel/plugin-proposal-do-expressions'
-    ]
+    plugins: ['react-native-reanimated/plugin']
 }
